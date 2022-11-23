93c52c0c7aafa97082e54825ac3aa3479e96952e
FahrezaDaffa93 <117092951+FahrezaDaffa93@users.noreply.github.com>
2022-11-23 09:44:10 +0700

    Add files via upload
    pemesanan

---
+++ pemesanan2.php
@@ -0,0 +1,103 @@
+<?php
+require('koneksi.php');
+if( isset($_POST['register']) ){
+    $nama_customer = $_POST['txt_nama_customer'];
+    $jenis_pelayanan = $_POST['txt_jenis_pelayanan'];
+    $harga = $_POST['txt_harga'];
+    $no_antrian = $_POST['txt_no_antrian'];
+    $tanggal_pemesanan= $_POST['txt_tanggal_pemesanan'];
+    
+    // query memasukkan data 
+
+    $query = "INSERT INTO pemesanan VALUES(null, '$nama_customer', '$jenis_pelayanan', '$harga','$no_antrian','$tanggal_pemesanan')";
+    $result = mysqli_query($koneksi, $query);
+    header('Location: pemesanan2.php');
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title> Register</title>
+
+    <!-- Custom fonts for this template-->
+    <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+    <link
+        href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i"
+        rel="stylesheet">
+
+    <!-- Custom styles for this template-->
+    <link href="css/sb-admin-2.min.css" rel="stylesheet">
+
+</head>
+
+<body class="bg-gradient-primary">
+
+    <div class="container">
+
+        <div class="card o-hidden border-0 shadow-lg" style="margin-top: 107px;">
+            <div class="card-body p-0">
+                <!-- Nested Row within Card Body -->
+                <div class="row">
+                    <div class="col-lg-5 d-none d-lg-block bg-register-image"></div>
+                    <div class="col-lg-7">
+                        <div class="p-5">
+                            <div class="text-center">
+                                <h1 class="h4 text-gray-900 mb-4">TAMBAH DATA KARYAWAN</h1>
+                            </div>
+                            <form class="user" action="pemesanan2.php" method="POST">
+                                <div class="form-group">
+                                    <input type="text" class="form-control form-control-user" id="exampleInputUsername"
+                                        placeholder="Nama" name="txt_nama_customer">
+                                </div>
+                                <div class="form-group">
+                                    <input type="text" class="form-control form-control-user" id="exampleInputUsername"
+                                        placeholder="Jenis_Pelayanan" name="txt_jenis_pelayanan">
+                                </div>
+                                <div class="form-group">
+                                    <input type="text" class="form-control form-control-user" id="exampleInputUsername"
+                                        placeholder="Harga" name="txt_harga">
+                                </div>
+                                <div class="form-group">
+                                    <input type="text" class="form-control form-control-user" id="exampleInputEmail"
+                                        placeholder="No Antrian " name="txt_no_antrian">
+                                </div>
+                                <div class="form-group">
+                                    <input type="password" class="form-control form-control-user" id="exampleInputPassword"
+                                        placeholder="Tanggal Pemesanan" name="txt_tanggal_pemesanan">
+                                </div>
+                                <button type="submit" name="register" class="btn btn-primary btn-user btn-block">TAMBAHKAN</button>
+                            </form>
+                            <hr>
+                            <div class="text-center">
+                                <a class="small" href="login.php"></a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </div>
+
+    <!-- Bootstrap core JavaScript -->
+    <script src="vendor/jquery/jquery.min.js"></script>
+    <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+    <!-- Core plugin JavaScript-->
+    <script src="vendor/jquery-easing/jquery.easing.min.js"></script>
+
+    <!-- Custom scripts for all pages-->
+    <script src="js/sb-admin-2.min.js"></script>
+
+</body>
+
+</html>
