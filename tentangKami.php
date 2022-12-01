9e4b42470becefefdff3359414f159d1191f2ac6
wulanayue <117131203+wulanayue@users.noreply.github.com>
2022-12-01 15:04:13 +0700

    Update tentangKami.php

--- tentangKami.php
+++ tentangKami.php
@@ -43,7 +43,7 @@
   <header id="header" class="header d-flex align-items-center">
     <div class="container-fluid container-xl d-flex align-items-center justify-content-between">
 
-      <a href="index.html" class="logo d-flex align-items-center">
+      <a href="index.php" class="logo d-flex align-items-center">
         <!-- Uncomment the line below if you also wish to use an image logo -->
         <!-- <img src="assets/img/logo.png" alt=""> -->
         <h1>CUTLUCK BARBERSHOP<span>.</span></h1>
@@ -51,29 +51,15 @@
 
       <i class="mobile-nav-toggle mobile-nav-show bi bi-list"></i>
       <i class="mobile-nav-toggle mobile-nav-hide d-none bi bi-x"></i>
-      <nav id="navbar" class="navbar">
+      <nav id="navbar" class="navbar" >
         <ul>
-        <li><a href="index.php" >Utama</a></li>
+          <li><a href="index.php">Utama</a></li>
           <li><a href="pelayanan.php">Pelayanan </a></li>
-          <li><a href="tentangKami.php" class="active">Tentang Kami </a></li>
+          <li><a href="tentangKami.php">Tentang Kami </a></li>
           <li><a href="projects.php">Galeri </a></li>
-          <li><a href="blog.php">Pesan </a></li>
-          <li><a href="blog.php">Kontak</a></li>
-          <li class="dropdown"><a href="#"><span>Dropdown</span> <i class="bi bi-chevron-down dropdown-indicator"></i></a>
-            <ul>
-              <li><a href="#">Dropdown 1</a></li>
-              <li class="dropdown"><a href="#"><span>DASHBOARD</span> <i class="bi bi-chevron-down dropdown-indicator"></i></a>
-                <ul>
-                  <li><a href="#">PEMESANAN</a></li>
-                  <li><a href="#">PENGELUARAN</a></li>
-                  <li><a href="#">PEMASUKAN</a></li>
-                  
-                </ul>
-              </li>
-              <li><a href="login.php">Logout</a></li>
-              
-            </ul>
-          </li>
+          <li><a href="kontak.php">Kontak</a></li>
+          <li><a href="dashboardPemesanan.php">Dashboard</a></li>
+          <li><a href="login.php">Logout</a></li>
         </ul>
       </nav><!-- .navbar -->
 
