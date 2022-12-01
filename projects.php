87fe779da3bbcc02a981b6e7af1512d563357cb6
wulanayue <117131203+wulanayue@users.noreply.github.com>
2022-12-01 15:04:10 +0700

    Update projects.php

--- projects.php
+++ projects.php
@@ -37,7 +37,7 @@
   <header id="header" class="header d-flex align-items-center">
     <div class="container-fluid container-xl d-flex align-items-center justify-content-between">
 
-      <a href="index.html" class="logo d-flex align-items-center">
+      <a href="index.php" class="logo d-flex align-items-center">
         <!-- Uncomment the line below if you also wish to use an image logo -->
         <!-- <img src="assets/img/logo.png" alt=""> -->
         <h1>CUTLUCK BARBERSHOP<span>.</span></h1>
@@ -45,22 +45,14 @@
 
       <i class="mobile-nav-toggle mobile-nav-show bi bi-list"></i>
       <i class="mobile-nav-toggle mobile-nav-hide d-none bi bi-x"></i>
-      <nav id="navbar" class="navbar">
+      <nav id="navbar" class="navbar" >
         <ul>
-          <li><a href="index.php" class="active">Utama</a></li>
-          <li><a href="pelayanan.php">Pelayanan</a></li>
-          <li><a href="tentangKami.php">Tentang Kami</a></li>
-          <li><a href="project.php">Galeri</a></li>
-          <li><a href="blog.php">Pesan </a></li>
-          <li><a href="blog.html">Kontak</a></li>
-          
-          <li class="dropdown"><a href="#"><span>DASHBOARD</span> <i class="bi bi-chevron-down dropdown-indicator"></i></a>
-            <ul>
-              <li><a href="dashboard.php">PEMESANAN </a></li>
-              <li><a href="#">PENGELUARAN</a></li>
-              <li><a href="#">PEMASUKAN</a></li>
-            </ul>
-          </li>
+          <li><a href="index.php">Utama</a></li>
+          <li><a href="pelayanan.php">Pelayanan </a></li>
+          <li><a href="tentangKami.php">Tentang Kami </a></li>
+          <li><a href="projects.php">Galeri </a></li>
+          <li><a href="kontak.php">Kontak</a></li>
+          <li><a href="dashboardPemesanan.php">Dashboard</a></li>
           <li><a href="login.php">Logout</a></li>
         </ul>
       </nav><!-- .navbar -->
