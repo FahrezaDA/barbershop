6bb48437561b06c6615e54207e06ccd40a28bce9
LUCKIWAHYUM <luckiwahyum0@gmail.com>
2022-12-01 13:54:49 +0700

    Update index.php

--- index.php
+++ index.php
@@ -57,7 +57,7 @@
           <li><a href="tentangKami.php">Tentang Kami </a></li>
           <li><a href="projects.php">Galeri </a></li>
           <li><a href="blog.php">Pesan </a></li>
-          <li><a href="contact.php">Kontak</a></li>
+          <li><a href="kontak.php">Kontak</a></li>
           <li><a href="dashboard.php">Dashboard</a></li>
           <li><a href="login.php">Logout</a></li>
         </ul>
@@ -1141,7 +1141,7 @@
           <li><a href="tentangKami.php">Tentang Kami </a></li>
           <li><a href="projects.php">Galeri </a></li>
           <li><a href="blog.php">Pesan </a></li>
-          <li><a href="contact.php">Kontak</a></li>
+          <li><a href="kontak.php">Kontak</a></li>
           <li><a href="dashboardPemesanan.php">Dashboard</a></li>
           <li><a href="login.php">Logout</a></li>
         </ul>
