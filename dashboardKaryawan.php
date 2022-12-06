23a38fc963b99e4971e2a6a3ea2b325764f23c3d
Khoiyimatul Hosniah <khoiyimatulhosniah23@gmail.com>
2022-12-06 22:31:18 +0700

    Update dashboardKaryawan.php

--- dashboardKaryawan.php
+++ dashboardKaryawan.php
@@ -12,8 +12,14 @@ $sesName = $_SESSION['name'];
 <html lang="en">
 
 <head>
+  <!-- Required meta tags -->
   <meta charset="utf-8">
-  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+  <!-- Bootstrap CSS -->
+  <link rel="stylesheet" href="assets2/css/bootstrap.min.css" >
+  <link rel="stylesheet" href="assets2/DataTables/DataTables-1.13.1/css/dataTables.bootstrap4.min.css">
+  <link rel="stylesheet" href="assets2/DataTables/Buttons-2.3.3/css/buttons.bootstrap4.min.css">
 
   <title>Barbershop</title>
   <meta content="" name="description">
@@ -402,9 +408,8 @@ $sesName = $_SESSION['name'];
                     <div class="container">
                         <div class="card mt-5">
                             <div class="card-body">
-                                <div class="table-responsive">
-                                <h3 class="display-6">Data Karyawan</h3>
-                                <table id="example" class="table table-bordered" style="width:100%">
+                                <h3 class="display-7">Data Karyawan</h3>
+                                <table id="data" class="table table-bordered" style="width:100%">
                                     <thead>
                                         <tr>
                                             <th>No</th>
@@ -508,6 +513,36 @@ $sesName = $_SESSION['name'];
             </div>
         </div>
     </div>
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="assets2/js/jquery.min.js"></script>
+    <script src="assets2/js/bootstrap.bundle.min.js"></script>
+
+    <!-- DataTables -->
+    <script src="assets2/DataTables/DataTables-1.13.1/js/jquery.dataTables.min.js"></script>
+    <script src="assets2/DataTables/DataTables-1.13.1/js/dataTables.bootstrap4.min.js"></script>
+
+
+    <script src="assets2/DataTables/Buttons-2.3.3/js/dataTables.buttons.min.js"></script>
+    <script src="assets2/DataTables/Buttons-2.3.3/js/buttons.bootstrap4.min.js"></script>
+    <script src="assets2/DataTables/JSZip-2.5.0/jszip.min.js"></script>
+    <script src="assets2/DataTables/pdfmake-0.1.36/pdfmake.min.js"></script>
+    <script src="assets2/DataTables/pdfmake-0.1.36/vfs_fonts.js"></script>
+    <script src="assets2/DataTables/buttons-2.3.3/js/buttons.html5.min.js"></script>
+    <script src="assets2/DataTables/buttons-2.3.3/js/buttons.print.min.js"></script>
+
+    <script>
+        $(document).ready(function() {
+            var table = $('#data').DataTable( {
+                lengthChange: false,
+                buttons: [ 'copy', 'print', 'excel', 'pdf',]
+            } );
+        
+            table.buttons().container()
+                .appendTo( '#data_wrapper .col-md-6:eq(0)' );
+        } );
+    </script>
 
     <!-- Bootstrap core JavaScript-->
     <script src="vendor/jquery/jquery.min.js"></script>
